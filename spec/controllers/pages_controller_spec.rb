437cf7440d806862a3187b6c5c0609c3da9b73ad
valentin <valentin.traen@gmail.com>
2014-08-15 17:51:33 +0200

    Fini avec les pages statiques

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, :type => :controller do
+
+  describe "GET home" do
+    it "returns http success" do
+      get :home
+      expect(response).to be_success
+    end
+
+  end
+
+  describe "GET contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to be_success
+    end
+
+  end
+
+  describe "GET about" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+
+  end
+
+
+end
